938bb12f15ea6d5705d252a293215a3aba986118
miketomotimo <82253169+miketomotimo@users.noreply.github.com>
2023-09-11 12:13:32 +1200

    Analyses the tweet corpus "favorite_tweets" and using NLTK, pandas, wxPython and many other libraries created a sentiment analyzer. This is done by assigning each tweet with a score and graphing it based on their date in chronological order. The user can provide a keyword that filters the corpus to only graph tweets that include that keyword.
    Changes:
    - Included a try except block in the case that csv file is lost, and provide instructions to return to retrieve it.

--- main.py
+++ main.py
@@ -18,26 +18,31 @@ Steps:
 
 
 def main():
-    # reads the favorite tweets csv
-    favourite_tweets = pd.read_csv("favorite-tweets.csv")
-    # dictionary to store the names and scores of tweets (name: score)
+    try:
+        # reads the favorite tweets csv
+        favourite_tweets = pd.read_csv("favorite-tweets.csv")
+        # dictionary to store the names and scores of tweets (name: score)
 
-    # Menu.main_menu()
+        # for tweet in favourite_tweets:
+        name_tweet_score = pd.DataFrame(columns=['UserName', 'Tweet', "Score"])
+        name_tweet_score['UserName'] = favourite_tweets['UserName']
+        name_tweet_score['Tweet'] = favourite_tweets['Text']
+        name_tweet_score.index = [Tools.string_to_datetime(date) for date in favourite_tweets['CreatedAt']]
+        name_tweet_score['Score'] = [sa.sentiment_processing(sa.tweet_formatter(tweet)) for tweet in
+                                     name_tweet_score['Tweet']]
 
-    # for tweet in favourite_tweets:
-    name_tweet_score = pd.DataFrame(columns=['UserName', 'Tweet', "Score"])
-    name_tweet_score['UserName'] = favourite_tweets['UserName']
-    name_tweet_score['Tweet'] = favourite_tweets['Text']
-    name_tweet_score.index = [Tools.string_to_datetime(date) for date in favourite_tweets['CreatedAt']]
-    name_tweet_score['Score'] = [sa.sentiment_processing(sa.tweet_formatter(tweet)) for tweet in name_tweet_score['Tweet']]
+        name_tweet_score = name_tweet_score.dropna(subset=['Tweet'])
 
-    name_tweet_score = name_tweet_score.dropna(subset=['Tweet'])
+        # GUI
+        app = wx.App()
+        Frame = MainFrame(None, name_tweet_score)
+        Frame.Show()
+        app.MainLoop()
 
-    # GUI
-    app = wx.App()
-    Frame = MainFrame(None,name_tweet_score)
-    Frame.Show()
-    app.MainLoop()
+    except:
+        # in the case that file is lost
+        print("File not Found, to recover file, re-download through the this app's github page at:"
+              " https://github.com/miketomotimo/Tweet_Sentiment_Analyzer")
 
 
 if __name__ == "__main__":
